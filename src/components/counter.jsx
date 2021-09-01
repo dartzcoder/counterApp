37a1c8239efa88e30c357acf0223632e7f09d705
Ubuntu <ubuntu@ubuntu.home>
2021-09-01 01:10:45 +0000

    added increment button

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,11 +5,27 @@ class Counter extends Component {
         count: 0,
         imgUrl: 'https://picsum.photos/200'
     };
+
+    handleIncrement = () => {
+        console.log("Increment");
+        this.setState((state)=>{
+            return {count: state.count + 1};
+        });
+    }
+    
     render(){
         return (<div>
                   <span className="badge bg-primary m-2">{this.formatCount()}</span>
-                  <button className="btn btn-secondary btn-sm">Increment</button>
+                  <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
                 </div>);
+    }
+
+   
+
+    getBadgeClasses(){
+        let classes = "badge m-2 badge-";
+        classes += this.state.count === 0 ? "warning" : "primary";
+        return classes;
     }
 
     formatCount(){
