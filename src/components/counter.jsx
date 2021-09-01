d7c13c446f65bfeb142cdd1d43f83cf6373099f3
Ubuntu <ubuntu@ubuntu.home>
2021-09-01 01:23:32 +0000

    updated

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,21 +6,17 @@ class Counter extends Component {
         imgUrl: 'https://picsum.photos/200'
     };
 
-    handleIncrement = () => {
-        console.log("Increment");
-        this.setState((state)=>{
-            return {count: state.count + 1};
-        });
+    handleIncrement = (product) => {
+        console.log(product);
+        this.setState({ count: this.state.count + 1 })
     }
     
     render(){
         return (<div>
                   <span className="badge bg-primary m-2">{this.formatCount()}</span>
-                  <button onClick={this.handleIncrement} className="btn btn-secondary btn-sm">Increment</button>
+                  <button onClick={ () => this.handleIncrement({ id: this.state.count }) } className="btn btn-secondary btn-sm">Increment</button>
                 </div>);
     }
-
-   
 
     getBadgeClasses(){
         let classes = "badge m-2 badge-";
