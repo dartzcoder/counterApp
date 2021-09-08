c49df9767ec3c02cc2ccf4cbd923f7a1e0876399
Ubuntu <ubuntu@ubuntu.home>
2021-09-08 01:43:30 +0000

    added multiple counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,31 +2,30 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = {
-        count: 0,
-        imgUrl: 'https://picsum.photos/200'
+        value: this.props.value,
     };
 
     handleIncrement = (product) => {
         console.log(product);
-        this.setState({ count: this.state.count + 1 })
-    }
-    
-    render(){
-        return (<div>
-                  <span className="badge bg-primary m-2">{this.formatCount()}</span>
-                  <button onClick={ () => this.handleIncrement({ id: this.state.count }) } className="btn btn-secondary btn-sm">Increment</button>
-                </div>);
+        this.setState({ value: this.state.value + 1 })
     }
 
-    getBadgeClasses(){
+    render() {
+        return (<div>
+            <span className="badge bg-primary m-2">{this.formatCount()}</span>
+            <button onClick={() => this.handleIncrement({ id: this.state.value })} className="btn btn-secondary btn-sm">Increment</button>
+        </div>);
+    }
+
+    getBadgeClasses() {
         let classes = "badge m-2 badge-";
-        classes += this.state.count === 0 ? "warning" : "primary";
+        classes += this.state.value === 0 ? "warning" : "primary";
         return classes;
     }
 
-    formatCount(){
-        const { count } = this.state
-        return count === 0 ? 'Zero' : count
+    formatCount() {
+        const { value } = this.state
+        return value === 0 ? 'Zero' : value
     }
 }
 
